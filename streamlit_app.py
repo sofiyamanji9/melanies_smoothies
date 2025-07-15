9be5b0ab8e7caa9dac33a8a1687de2dafde4164a
sofiyamanji9 <sofiya.manji@albertahealthservices.ca>
2025-07-14 18:23:07 -0600

    Update streamlit_app.py
    added API information

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,10 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
